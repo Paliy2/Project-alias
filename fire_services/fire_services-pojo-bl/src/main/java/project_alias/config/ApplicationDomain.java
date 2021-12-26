eca08186e0bae95bba6b7cd67912c36386b45959
Oleksandr Dubas <60939773+saniochky@users.noreply.github.com>
2021-12-26 18:57:36 +0200

    Fixed unnessesary whitespaces manually

--- fire_services/fire_services-pojo-bl/src/main/java/project_alias/config/ApplicationDomain.java
+++ fire_services/fire_services-pojo-bl/src/main/java/project_alias/config/ApplicationDomain.java
@@ -29,36 +29,36 @@ import project_alias.form_items.FormTypeItem;
  * 
  */
 public class ApplicationDomain implements IApplicationDomainProvider {
-	private static final Set<Class<? extends AbstractEntity<?>>> entityTypes = new LinkedHashSet<>();
-	private static final Set<Class<? extends AbstractEntity<?>>> domainTypes = new LinkedHashSet<>();
+    private static final Set<Class<? extends AbstractEntity<?>>> entityTypes = new LinkedHashSet<>();
+    private static final Set<Class<? extends AbstractEntity<?>>> domainTypes = new LinkedHashSet<>();
 
-	static {
-		entityTypes.addAll(PlatformDomainTypes.types);
-		add(Person.class);
-		add(EquipmentClass.class);
-		add(Status.class);
-		add(VehicleType.class);
-		add(EquipmentType.class);
-		add(Equipment.class);
-		add(Vehicle.class);
-		add(Role.class);
-		add(OpenEquipmentClassMasterAction.class);
-		add(EquipmentClassMaster_OpenMain_MenuItem.class);
-		add(EquipmentClassMaster_OpenEquipmentType_MenuItem.class);
-		add(FormTypeItem.class);
-	}
+    static {
+	entityTypes.addAll(PlatformDomainTypes.types);
+	add(Person.class);
+	add(EquipmentClass.class);
+	add(Status.class);
+	add(VehicleType.class);
+	add(EquipmentType.class);
+	add(Equipment.class);
+	add(Vehicle.class);
+	add(Role.class);
+	add(OpenEquipmentClassMasterAction.class);
+	add(EquipmentClassMaster_OpenMain_MenuItem.class);
+	add(EquipmentClassMaster_OpenEquipmentType_MenuItem.class);
+	add(FormTypeItem.class);
+    }
 
-	private static void add(final Class<? extends AbstractEntity<?>> domainType) {
-		entityTypes.add(domainType);
-		domainTypes.add(domainType);
-	}
+    private static void add(final Class<? extends AbstractEntity<?>> domainType) {
+	entityTypes.add(domainType);
+	domainTypes.add(domainType);
+    }
 
-	@Override
-	public List<Class<? extends AbstractEntity<?>>> entityTypes() {
-		return Collections.unmodifiableList(entityTypes.stream().collect(Collectors.toList()));
-	}
+    @Override
+    public List<Class<? extends AbstractEntity<?>>> entityTypes() {
+	return Collections.unmodifiableList(entityTypes.stream().collect(Collectors.toList()));
+    }
 
-	public List<Class<? extends AbstractEntity<?>>> domainTypes() {
-		return Collections.unmodifiableList(domainTypes.stream().collect(Collectors.toList()));
-	}
+    public List<Class<? extends AbstractEntity<?>>> domainTypes() {
+	return Collections.unmodifiableList(domainTypes.stream().collect(Collectors.toList()));
+    }
 }
